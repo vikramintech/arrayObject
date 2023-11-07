909bf328c93daecb158f7239fc9e7c002d2503ce
Vikram Singh Rana <116163333+vikramintech@users.noreply.github.com>
2023-11-07 15:26:34 +0000

    js code added

--- index.js
+++ index.js
@@ -1,2 +1,58 @@
 console.clear();
 
+const cars = [
+  {
+    carSegment: "Hatchback",
+    manufacturName: "Maruti Suzuki",
+    carName: "Swift",
+    safetyRatings: 1,
+    isSafe: false,
+  },
+  {
+    carSegment: "Sedan",
+    manufacturName: "Hyundai",
+    carName: "verna",
+    safetyRatings: 5,
+    isSafe: true,
+  },
+  {
+    carSegment: "Sedan",
+    manufacturName: "Honda",
+    carName: "City",
+    safetyRatings: 4,
+    isSafe: true,
+  },
+  {
+    carSegment: "Hatchback",
+    manufacturName: "VW",
+    carName: "Polo",
+    safetyRatings: 5,
+    isSafe: true,
+  },
+  {
+    carSegment: "Hatchback",
+    manufacturName: "TATA",
+    carName: "Nexon",
+    safetyRatings: 5,
+    isSafe: true,
+  },
+];
+console.log(
+  `${cars[0].carName} is of ${cars[0].carSegment} segment and its NCAP safety rating is ${cars[0].safetyRatings} star. So it is ${cars[0].isSafe} that this car is safer car.`
+);
+console.log(
+  `${cars[1].carName} is of ${cars[1].carSegment} segment and its NCAP safety rating is ${cars[1].safetyRatings} star. So it is ${cars[1].isSafe} that this car is safer car.`
+);
+console.log(
+  `${cars[2].carName} is of ${cars[2].carSegment} segment and its NCAP safety rating is ${cars[2].safetyRatings} star. So it is ${cars[2].isSafe} that this car is safer car.`
+);
+console.log(
+  `${cars[3].carName} is of ${cars[3].carSegment} segment and its NCAP safety rating is ${cars[3].safetyRatings} star. So it is ${cars[3].isSafe} that this car is safer car.`
+);
+console.log(
+  `${cars[4].carName} is of ${cars[4].carSegment} segment and its NCAP safety rating is ${cars[4].safetyRatings} star. So it is ${cars[4].isSafe} that this car is safer car.`
+);
+
+console.log(
+  `${cars[0].carName} is of ${cars[0].carSegment} segment and its NCAP safety ratings is ${cars[0].safetyRatings} star. So it is ${car[0].isSafe} that this car is safer car.`
+);
